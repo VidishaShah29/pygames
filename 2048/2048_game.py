6d35ea253fc78864981f93738007134549e08f4b
Jiri Novotny <fg-42@seznam.cz>
2016-06-30 18:53:18 +0200

    removed unused code

--- 2048/2048_game.py
+++ 2048/2048_game.py
@@ -11,31 +11,6 @@ pygame.display.set_caption("2048")
 board = Board(4, 4, max_random_value=4)
 print(board.matrix)
 
-"""
-board.matrix = np.array(
-    [[4, 2, 0, 2, 0],
-    [0, 2, 0, 0, 0],
-    [4, 0, 0, 0, 0],
-    [0, 2, 0, 0, 0],
-    [4, 8, 0, 0, 0]]
-)
-
-
-board.matrix = np.array(
-    [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-)
-
-print(board.matrix)
-board.move("up")
-board.insert_random_tile()
-print()
-print(board.matrix)
-"""
-
 FONT = pygame.font.SysFont("Arial", 12)
 
 BLACK = (0, 0, 0)
@@ -144,14 +119,6 @@ while True:
                       WIDTH,
                       HEIGHT],
                      FONT)
-            """
-            pygame.draw.rect(screen,
-                             color,
-                             [(MARGIN + WIDTH) * column + MARGIN,
-                              (MARGIN + HEIGHT) * row + MARGIN,
-                              WIDTH,
-                              HEIGHT])
-            """
 
     clock.tick(60)
     pygame.display.flip()
